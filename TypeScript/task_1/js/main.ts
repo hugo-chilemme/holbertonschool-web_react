70ae99f8ae1e6476c08cbfd7b36273aa4884dbf2
Hugo Chilemme <hugo.chilemme@gmail.com>
2025-06-06 09:03:30 +0000

    Ajout de la fonctionnalité d'affichage des étudiants avec une classe StudentClass et une fonction printTeacher

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -30,3 +30,38 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); 
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    constructor(private firstName: string, private lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student = new StudentClass("Jane", "Doe");
+console.log(student.displayName());
+console.log(student.workOnHomework());
